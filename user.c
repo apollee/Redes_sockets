456bd83f56fcb64c8a98092eb19549b6376fb9db
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2019-09-22 12:26:16 +0100

    added parse of input actions with strtok

--- user.c
+++ user.c
@@ -11,14 +11,15 @@
 /*------------------------*/
 
 int input_command();
-int input_action();
+int parse_input_action();
+void input_action();
 
 /*------------------------*/
 
 
 int main(int argc, char *argv[]) {
     input_command(argc, argv);
-    input_action();
+    parse_input_action();
 }
 
 int input_command(int argc, char *argv[]) {
@@ -40,4 +41,31 @@ int input_command(int argc, char *argv[]) {
         return -1;
     }
     return 0;
+}
+
+int parse_input_action() {
+    int numTokens = 0;
+    char *saveTokens[7];
+    char input[50];
+    
+    if(fgets(input, 50, stdin) == NULL){
+        return -1;
+    }    
+    
+    input[strcspn(input, "\n")] = 0; /*remove the \n added by fgets*/
+    char *token = strtok(input, " ");
+
+    while(token != NULL) {
+        saveTokens[numTokens] = token;
+        numTokens++;
+        token = strtok(NULL, " ");
+    }
+    input_action(numTokens, saveTokens);
+}
+
+void input_action(int numTokens, char** saveTokens) {
+    int i;
+    for(i = 0; i < numTokens; i++){
+        printf("%s\n", saveTokens[i]);
+    }
 }
